a6374cc8ad8ca4e3e2710aa24f6525863823d90f
Josef Dosoudil <73310547+jeptun@users.noreply.github.com>
2022-02-09 01:04:39 +0100

    Update script.js

--- MobileApp/Javascript/script.js
+++ MobileApp/Javascript/script.js
@@ -26,7 +26,22 @@ function toggleDiv() {
     }
   }
 }
+/*
+var dropdown = document.getElementsByClassName("dropdown-rip");
+var i;
 
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+*/
 /* Funkce na změnu animace Hamburgermenu  */
 function flipIcon(x) {
   x.classList.toggle("change");
